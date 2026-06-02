febac6e186e272c9d64e9a81e976bb002f34ed73
Latwun <162395437+WendyLind@users.noreply.github.com>
2026-06-02 14:40:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,14 @@
-document.querySelectorAll(".stars").forEach(container => {
-
-  const rating =
-    parseInt(container.dataset.rating);
-
-  for(let i = 1; i <= 5; i++){
-
-    const star =
-      document.createElement("img");
-
-    star.src = "img/star.jpg";
-
-    if(i <= rating){
-      star.classList.add("active");
-    }
-
-    container.appendChild(star);
-  }
-
-});
-
-const track =
-  document.getElementById("track");
-
-const movieScroll =
-  document.getElementById("movieScroll");
-
-track.innerHTML += track.innerHTML;
-
-setInterval(() => {
-
-  if(!movieScroll.matches(":hover")){
-
-    movieScroll.scrollTop += 0.4;
-
-    if(
-      movieScroll.scrollTop >=
-      track.scrollHeight / 2
-    ){
-      movieScroll.scrollTop = 0;
-    }
-
-  }
-
-},16);
-
-const tabs =
-  document.querySelectorAll(".tab");
-
-const contents =
-  document.querySelectorAll(".tab-content");
-
-tabs.forEach(tab => {
+document.querySelectorAll(".tab").forEach(tab => {
 
   tab.addEventListener("click", () => {
 
-    tabs.forEach(t =>
-      t.classList.remove("active")
-    );
+    document
+      .querySelectorAll(".tab")
+      .forEach(t => t.classList.remove("active"));
 
-    contents.forEach(c =>
-      c.classList.remove("active")
-    );
+    document
+      .querySelectorAll(".tab-content")
+      .forEach(c => c.classList.remove("active"));
 
     tab.classList.add("active");
 
@@ -69,25 +17,5 @@ tabs.forEach(tab => {
       .classList.add("active");
 
   });
-
-});
-
-const topSection =
-  document.getElementById("topSection");
-
-const bottomSection =
-  document.getElementById("bottomSection");
-
-bottomSection.addEventListener("scroll", () => {
-
-  if(bottomSection.scrollTop > 30){
-
-    topSection.classList.add("collapsed");
-
-  }else{
-
-    topSection.classList.remove("collapsed");
-
-  }
 
 });
