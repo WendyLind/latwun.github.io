4eec2e16cc3203778b5b6af077b97c9eb4ff5f17
Latwun <162395437+WendyLind@users.noreply.github.com>
2026-06-02 15:42:03 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+```javascript
+document.querySelectorAll(".stars").forEach(container => {
+
+    const rating =
+    parseInt(container.dataset.rating);
+
+    for(let i = 1; i <= 5; i++){
+
+        const star =
+        document.createElement("img");
+
+        star.src = "img/star.jpg";
+
+        if(i <= rating){
+            star.classList.add("active");
+        }
+
+        container.appendChild(star);
+    }
+
+});
+
+const track =
+document.getElementById("track");
+
+const movieScroll =
+document.getElementById("movieScroll");
+
+track.innerHTML += track.innerHTML;
+
+setInterval(() => {
+
+    if(!movieScroll.matches(":hover")){
+
+        movieScroll.scrollTop += 0.4;
+
+        if(
+            movieScroll.scrollTop >=
+            track.scrollHeight / 2
+        ){
+            movieScroll.scrollTop = 0;
+        }
+
+    }
+
+},16);
+```
